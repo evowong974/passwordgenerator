2fd1285430198b6653ce9b4722fc11dfac1c8c43
evowong974 <131270489+evowong974@users.noreply.github.com>
2023-04-23 18:40:16 -0400

    userpasswordgenerator

--- src/main/java/edu/guilford/UserControl.java
+++ src/main/java/edu/guilford/UserControl.java
@@ -6,53 +6,68 @@ public class UserControl
     {
     // Instantiate a User object with information provided by the user
 
-    User user1 = new User("John", "Doe", "doe232@guilford.edu", "blue", 42, "dog");
+    User user1 = new User();
+    user1.obtainUser1Info();
+    
 
     // Instantiate the second User object with information provided by the user
 
-    User user2 = new User("Jane", "Sans", "sans@guilford.edu", "lavender", 13, "cat");
+    User user2 = new User();
+    user2.obtainUser2Info(); 
 
-    // Print the first name, last name, email, favorite color, favorite number and favorite animal for the first and second User object
-
-    System.out.println("The first name and last name for the first user is: " + user1.getFirstName() + " " + user1.getLastName() + " " + "\n" + "First user's email: " + user1.getEmail() + " " + "\n" + "First user's favorite color: " + user1.getFavoriteColor() + " " + "\n" + "First user's favorite number: " + user1.getFavoriteNumber() + " " + "\n" + "First user's favorite animal: " + user1.getFavoriteAnimal());
     
-    System.out.println("The first name and last name for the second user is: " + user2.getFirstName() + " " + user2.getLastName() + " " + "\n" + "Second user's email: " + user2.getEmail() + " " + "\n" + "Second user's favorite color: " + user2.getFavoriteColor() + " " + "\n" + "Second user's favorite number: " + user2.getFavoriteNumber() + " " + "\n" + "Second user's favorite animal: " + user2.getFavoriteAnimal());
-    
-
-
     // Generate the password for the first and second User objects based on their first name, last name, favorite color and favorite number
     
-    String password1 = user1.generatePassword("John", "Doe", "doe232@guilford.edu", 12, "cat");
+    user1.generatePassword();
 
-    String password2 = user2 .generatePassword("Jane", "Sans", "sans@guilford.edu", 13, "dog");
+    user2.generatePassword();
+
+    // Print out the generated passwords for the first and second User objects
+
+    System.out.println("\n" + "The password for the first user is: " + user1.getThePassword());
+
+    System.out.println("The password for the second user is: " + user2.getThePassword());
+    
+    // Set the last 3 characters of the password as the secret key for encryption
+
+    String secretPassword1 = user1.getThePassword().substring(user1.getThePassword().length() - 3);
+
+    String secretPassword2 = user2.getThePassword().substring(user2.getThePassword().length() - 3);
+
+    // Print out the two secret keys
+
+    System.out.println("\n" + "The secret key for the first user is: " + secretPassword1);
+
+    System.out.println("The secret key for the second user is: " + secretPassword2);
+
 
 
     // Encrypt the password for the first and second User object using the AES class
+ 
+    String encryptPassword1 = AES.encrypt(user1.getThePassword(), secretPassword1);
 
-    String encryptedPassword1 = AES.encrypt(password1, "password");
-
-    String encryptedPassword2 = AES.encrypt(password2, "password");
-
-
-    // Print the password for the first User object and the second User object
-
-    System.out.println("The password for the first user is: " + password1);
-
-    System.out.println("The password for the second user is: " + password2);
+    String encryptPassword2 = AES.encrypt(user2.getThePassword(), secretPassword2);
 
 
-    // Print the encrypted password for the first User object and the second User object
+    // Print out the encrypted passwords for the first and second User objects
 
-    System.out.println("The encrypted password for the first user is: " + encryptedPassword1);
+    System.out.println("\n" + "The encrypted password for the first user is: " + encryptPassword1);
 
-    System.out.println("The encrypted password for the second user is: " + encryptedPassword2);
+    System.out.println("The encrypted password for the second user is: " + encryptPassword2);
+
+
+    // Decrypt the password for the first and second User object using the AES class
+
+    String decryptPassword1 = AES.decrypt(encryptPassword1, secretPassword1);
+
+    String decryptPassword2 = AES.decrypt(encryptPassword2, secretPassword2);
 
 
     // Print the decrypted password for the first User object and the second User object
 
-    System.out.println("The decrypted password for the first user is: " + AES.decrypt(encryptedPassword1, "password"));
+    System.out.println("\n" + "The decrypted password for the first user is: " + decryptPassword1);
 
-    System.out.println("The decrypted password for the second user is: " + AES.decrypt(encryptedPassword2, "password"));
+    System.out.println("The decrypted password for the second user is: " + decryptPassword2);
 
 
 
